0513b517ad310eaac7a3160f2b12e05186f6d321
Xcel Tech <ximeng.zhang@uqconnect.edu.au>
2024-11-13 09:09:47 +1000

    first update

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+@app.route('/form')
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'name is {name}'
+
+
+if __name__ == '__main__':
+    app.run()
